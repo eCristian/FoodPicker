57694ab1b655a0943a502e5cfa9e9c1e6b7682e2
eCristian <63387703+eCristian@users.noreply.github.com>
2020-06-12 11:19:00 +0300

    added generation part

--- src/components/Food/GenerateFood/GenerateFood.js
+++ src/components/Food/GenerateFood/GenerateFood.js
@@ -11,6 +11,13 @@ import logo from '../../BaseFiles/logo.png';
 
 
 export class GenerateFood extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      result: ""
+    }
+  }
 
   handleChooseFoodAgain = () => {
     this.props.history.push('/chooseFood')
@@ -21,16 +28,21 @@ export class GenerateFood extends React.Component {
     this.props.history.push('/')
   }
 
-  componentDidMount = () => {
-    const numberOfProducts = 3;
-    const randomIndex = Math.floor(Math.random() * numberOfProducts);
-    const limit = 1;
+  componentWillMount = () => {
+    let dbRef = fire.database().ref(`${this.props.location.state.whatToEat}/${this.props.location.state.whenServed}`);
 
-    var reference = fire.database().ref(`${this.props.location.state.whatToEat}/${this.props.location.state.whenServed}`);
+    let productsCounter = 0;
 
-    reference.orderByChild("Id").startAt(2).limitToFirst(limit).once('value').then(snapshot => {
-      //var data = snap.val();
-      console.log(snapshot.val());
+    dbRef.orderByChild("Name").on("child_added", function(data) {
+      ++productsCounter;
+    });
+
+    const randomIndex = Math.floor(Math.random() * productsCounter);
+
+    dbRef.orderByChild("Id").startAt(randomIndex).limitToFirst(1).once("child_added", snapshot => {
+      let data = snapshot.val();
+      this.setState({ result: data.Name });
+      console.log(this.state.result);
     })
   }
 
@@ -38,7 +50,8 @@ export class GenerateFood extends React.Component {
     return (
       <div>
         <Link to="/"><img id="logo" src={logo} alt="Logo" /></Link>
-        <p id="resultText">You chose {this.props.location.state.whatToEat} for {this.props.location.state.whenServed}!</p>
+        <p id="parametersText">You chose {this.props.location.state.whatToEat} for {this.props.location.state.whenServed}!</p>
+        <h1 id="resultText">{this.state.result}</h1>
         <button id="chooseFoodAginBtn" type="button" class="btn btn-success" onClick={this.handleChooseFoodAgain}>Choose Food Again</button>
         <p id="footerTextGenerateFood">You are logged in as User.</p>
         <button id="logoutBtn" type="button" class="btn btn-danger" onClick={this.handleLogout}>Logout</button>
